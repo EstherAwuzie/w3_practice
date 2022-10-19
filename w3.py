d69baad57b263c9e8f724240e5b6482510819521
EstherAwuzie <awuziechiamaka@gmail.com>
2022-10-19 20:25:04 +0100

    Add new file; w3.py

---
+++ w3.py
@@ -0,0 +1,56 @@
+# Program that accepts the user's first and last name 
+# and print them in reverse order with a space between them.
+
+# fname=input("What is your fisrt name:")
+# lname=input("What is your last name:")
+# print(lname, fname)
+
+# OR
+
+# prompt="Write your full name here. In this format: firstname lastname\n"
+# inp=input(prompt)
+# ins,ihs=inp.split(" ")
+# print(ihs,ins)
+
+# OR
+
+# name=input("write your full name:")
+# fname=name.split()
+# print(fname[1], fname[0])
+
+# Program to calculate the sum of three given numbers,
+# if the values are equal then return three times of their sum
+
+# fname=int(input("first number:"))
+# sname=int(input("second name:"))
+# tname=int(input("third name:"))
+# yes=fname+sname+tname
+# no=yes*3
+# if fname==sname==tname:
+#     print(no)
+# else:
+#     print("Try inputing the same numbers.\nThanks")
+
+# Program to find whether a given number is even or odd
+# print out an appropraite message to the user.
+
+# num=int(input("Input a number:"))
+# if num % 2 ==0:
+#     print("Yes! An even number")
+# else:
+#     print("Oops! An odd number")
+
+# Program to count the number 4 in a given list.
+
+# number=[4,2,1,4,0,4,1,4,2]
+# c=number.count(4)
+# print(c)
+
+# Program to test whether a passed letter is a vowel or not.
+
+# vowels=['a', 'e', 'i', 'o', 'u']
+# inp=input("input any letter:")
+# if inp in vowels:
+#     print(inp, "is a vowel")
+# else:
+#     print(inp, "is not a vowel")
